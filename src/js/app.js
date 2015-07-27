18d9353e2ebc2a57ee2d1c1c6bbd9ac35723b60a
Yevgen Motrych <Yevgen.Motrych@medhost.com>
2015-07-27 19:00:14 +0300

    added ui router

--- src/js/app.js
+++ src/js/app.js
@@ -6,31 +6,38 @@ angular.module('footballInfo', ['ui.router'])
 
 		$stateProvider
 			.state('dashboard', {
+				url: '/dashboard',
 				templateUrl: 'templates/dashboard.html',
 				controller: 'DashboardCtrl'
 			})
-			.state('championships.:id', {
-				templateUrl: 'templates/detailed-championship.html',
-				controller: 'DetailedChampionshipCtrl'
-			})
 			.state('championships', {
+				url: '/championships',
 				templateUrl: 'templates/championships.html',
 				controller: 'ChampionshipsCtrl'
 			})
-			.state('matches/:id', {
-				templateUrl: 'templates/detailed-match.html',
-				controller: 'DetailedMatchCtrl'
+			.state('championship', {
+				url: '/championships/:id',
+				templateUrl: 'templates/detailed-championship.html',
+				controller: 'DetailedChampionshipCtrl'
 			})
 			.state('matches', {
+				url: '/matches',
 				templateUrl: 'templates/matches.html',
 				controller: 'MatchesCtrl'
 			})
-			.state('teams/:id', {
-				templateUrl: 'templates/detailed-team.html',
-				controller: 'DetailedTeamCtrl'
+			.state('match', {
+				url: '/matches/:id',
+				templateUrl: 'templates/detailed-match.html',
+				controller: 'DetailedMatchCtrl'
 			})
 			.state('teams', {
+				url: '/teams',
 				templateUrl: 'templates/teams.html',
 				controller: 'TeamsCtrl'
+			})
+			.state('team', {
+				url: '/teams/:id',
+				templateUrl: 'templates/detailed-team.html',
+				controller: 'DetailedTeamCtrl'
 			});
 	});
