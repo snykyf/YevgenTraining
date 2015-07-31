e45506a6cdfba8a592e71abf90d9b9ae13eeb3fa
Yevgen Motrych <Yevgen.Motrych@medhost.com>
2015-07-31 15:06:32 +0300

    added teams sorting

--- src/js/controllers/TeamsCtrl.js
+++ src/js/controllers/TeamsCtrl.js
@@ -7,12 +7,17 @@ angular.module('footballInfo')
 			$scope.teams = teamsArr;
 			$scope.championships = championshipsArr;
 			$scope.emblemBaseUrl = 'http://footballbet.com.ua/teams/embl/';
+			$scope.sortingOrder = false;
 
 			$scope.getChampionship = function (id) {
 				//TODO: try to find out how to call service method here
 				return this.championships.filter(function (championship) {
 					return championship['id_championship'] === id;
 				})[0];
+			};
+
+			$scope.setSortingOrder = function () {
+				$scope.sortingOrder = !$scope.sortingOrder;
 			};
 		}
 	]);
