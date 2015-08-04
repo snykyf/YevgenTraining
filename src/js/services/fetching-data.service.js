73f6f45637b3f687e16dc758f913f53c0a9b4989
i10111pn <i10111pn@gmail.com>
2015-08-04 16:18:35 +0300

    fixed team detailed

--- src/js/services/fetching-data.service.js
+++ src/js/services/fetching-data.service.js
@@ -1,106 +1,109 @@
 // A RESTful factory for retrieving data
 angular.module('footballInfo')
-	.factory('fetchingDataService', ['$http', function ($http) {
-		'use strict';
+	.factory('fetchingDataService', ['$http', '$q',
+        function ($http, $q) {
+            'use strict';
 
-		//config objects
-		var championships = {
-			url: 'http://footballbet.com.ua/api/championships/',
-			idName: 'id_championship'
-		};
+            //config objects
+            var championships = {
+                url: 'http://footballbet.com.ua/api/championships/',
+                idName: 'id_championship'
+            };
 
-		var teams = {
-			url: 'http://footballbet.com.ua/api/teams/',
-			idName: 'id_teams'
-		};
+            var teams = {
+                url: 'http://footballbet.com.ua/api/teams/',
+                idName: 'id_teams'
+            };
 
-		var matches = {
-			url: 'http://footballbet.com.ua/api/matches/',
-			idName: 'idMatch'
-		};
+            var matches = {
+                url: 'http://footballbet.com.ua/api/matches/',
+                idName: 'idMatch'
+            };
 
-		//helper methods
-		function fetchAllItems (store) {
-            return $http
-                .get(store.url, {cache: true})
-                .then(function (resp) {
-                    return resp.data.result;
+            //helper methods
+            function fetchAllItems (store) {
+                return $http
+                    .get(store.url, {cache: true})
+                    .then(function (resp) {
+                        return resp.data.result;
+                    });
+            }
+
+            function findById (store, id, idName) {
+                store = store.data || store;
+
+                //TODO: add break
+                return store.filter(function (item) {
+                    return item[idName] === id;
+                })[0];
+            }
+
+            //service
+            var factory = {};
+
+            factory.getAllChampionships = function () {
+                return fetchAllItems(championships);
+            };
+
+            factory.getChampionship = function (id) {
+                return this
+                    .getAllTeams()
+                    .then(function (teamsArr) {
+                        return teamsArr[id];
+                    });
+            };
+
+            factory.getAllTeams = function () {
+                return fetchAllItems(teams)
+                    .then(function (teamsArr) {
+                        // get unique array of id_championship
+                        var championshipsId = {},
+                            teams = [];
+
+                        teamsArr.forEach(function (team) {
+                            championshipsId[team.id_championship] = null;
+                        });
+                        championshipsId = Object.keys(championshipsId);
+
+                        // sort teams by county
+                        championshipsId.forEach(function (championshipId) {
+                            teams[championshipId] = teamsArr.filter(function (team) {
+                                return team.id_championship == championshipId;
+                            });
+                        });
+
+                        return teams;
                 });
-        }
+            };
 
-		function findById (store, id, idName) {
-			store = store.data || store;
+            factory.getTeam = function (id) {
+                return this
+                    .getAllTeams()
+                    .then(function (teamsArr) {
+                        var result;
 
-			//TODO: add break
-			return store.filter(function (item) {
-				return item[idName] === id;
-			})[0];
-		}
-
-		//service
-		var factory = {};
-
-		factory.getAllChampionships = function () {
-			return fetchAllItems(championships);
-		};
-
-		factory.getChampionship = function (id) {
-			return this
-				.getAllTeams()
-				.then(function (teamsArr) {
-					return teamsArr[id];
-				});
-		};
-
-		factory.getAllTeams = function () {
-			return fetchAllItems(teams)
-				.then(function (teamsArr) {
-					// get unique array of id_championship
-					var championshipsId = {},
-						teams = [];
-
-					teamsArr.forEach(function (team) {
-						championshipsId[team.id_championship] = null;
-					});
-					championshipsId = Object.keys(championshipsId);
-
-					// sort teams by county
-					championshipsId.forEach(function (championshipId) {
-						teams[championshipId] = teamsArr.filter(function (team) {
-							return team.id_championship == championshipId;
-						});
-					});
-
-					return teams;
-			});
-		};
-
-		factory.getTeam = function (id) {
-			var result;
-            this.getAllTeams()
-                .then(function (teamsArr) {
-                    for (var index in teamsArr) {
-                        result = findById(teamsArr[index], id, teams.idName);
-                        if (result) {
-                            break;
+                        for (var index in teamsArr) {
+                            result = findById(teamsArr[index], id, teams.idName);
+                            if (result) {
+                                break;
+                            }
                         }
-                    }
-                    return result;
-            });
-		};
 
-		factory.getAllMatches = function () {
-			return fetchAllItems(matches);
-		};
+                        return result;
+                    });
+            };
 
-		factory.getMatch = function (id) {
-			return this
-                .getAllMatches()
-                .then(function (matchesArr) {
-                    debugger;
-                    return findById(matchesArr, id, matches.idName);
-                });
-        };
+            factory.getAllMatches = function () {
+                return fetchAllItems(matches);
+            };
 
-		return factory;
+            factory.getMatch = function (id) {
+                return this
+                    .getAllMatches()
+                    .then(function (matchesArr) {
+                        return findById(matchesArr, id, matches.idName);
+                    });
+            };
+
+            return factory;
 	}]);
